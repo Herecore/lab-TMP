121f84242fcedb68c0b75c4aeed1a23328939c77
herecore <herecore@protonmail.com>
2022-11-20 14:10:54 +0300

    Started solving problem 11 of lab 5

---
+++ lab-5/problem-11/program.cpp
@@ -0,0 +1,20 @@
+/* Лабораторная, Строки. Задание 11: 
+Составить программу, в результате выполнения которой из первой
+заданной строки удаляется каждый символ, не принадлежащий также
+второй строке
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string S1, S2;
+	cout << "Enter the first string: ";
+	cin >> S1;
+	cout << "Enter the second string: ";
+	cin >> S2;
+	
+
+}
